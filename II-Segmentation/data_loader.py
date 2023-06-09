6f38cda7d1149f1ff8a9849ae457f24f3bcb8316
jakwisn <jakwisn@gmail.com>
2023-06-09 12:44:27 +0200

    add data loader

---
+++ II-Segmentation/data_loader.py
@@ -0,0 +1,54 @@
+
+import os
+import numpy as np
+import glob
+import PIL.Image as Image
+
+# pip install torchsummary
+import torch
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from time import time
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+
+data_path = '/dtu/datasets1/02514/PH2_Dataset_images/'
+class PH2(torch.utils.data.Dataset):
+    def __init__(self, train, transform, data_path=data_path, train_size=0.66):
+        'Initialization'
+        self.transform = transform
+        files = glob.glob(data_path + '*')
+        
+        np.random.seed(420)
+        train_files = np.random.choice(files, int(train_size * len(files)), replace=False) 
+        test_files = [f for f in files if f not in train_files]
+
+        if train:
+            files_to_read = train_files
+        else:
+            files_to_read = test_files
+
+        img_ids = pd.Series(files_to_read).str.split('/').apply(lambda x: x[-1])
+        img_path   = data_path + img_ids +'/'+ img_ids + '_Dermoscopic_Image/' + img_ids + '.bmp'
+        label_path = data_path + img_ids +'/'+ img_ids + '_lesion/' + img_ids + '_lesion.bmp'
+        
+        self.image_paths = sorted(img_path)
+        self.label_paths = sorted(label_path)
+        
+    def __len__(self):
+        'Returns the total number of samples'
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        'Generates one sample of data'
+        image_path = self.image_paths[idx]
+        label_path = self.label_paths[idx]
+        
+        image = np.array(plt.imread(image_path))
+        # taking the first channel as they are the same
+        label = np.array(plt.imread(label_path)[:,:,0])
+        Y = self.transform(label)
+        X = self.transform(image)
+        return X, Y
