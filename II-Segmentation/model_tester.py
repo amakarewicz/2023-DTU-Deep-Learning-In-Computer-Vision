4b4710657a36655955c0b99b89e714b9c2f447c7
Tobias Munch <s183478@student.dtu.dk>
2023-06-14 10:57:48 +0200

    added model tester

---
+++ II-Segmentation/model_tester.py
@@ -0,0 +1,13 @@
+import torch
+import matplotlib as plt
+
+def test_model(model):
+    main_path = "/dtu/dataset1/02514/"
+
+    ph2_paths ["PH2_Dataset_images/IMD002.bmp", "PH2_Dataset_images/IMD003.bmp", "PH2_Dataset_images/IMD014.bmp"]
+    drive_paths = ["DRIVE/test/images/01_test.tif", "DRIVE/test/images/02_test.tif", "DRIVE/test/images/03_test.tif"]
+
+    for 
+
+    
+    
