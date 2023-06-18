deca4fbbb29c59236ac715b726685c93235adeb1
jacekchess <j.wisniewski@vp.pl>
2023-06-18 21:59:34 +0200

    Dataloader fix

--- IV-Object-Detection/data_loader.py
+++ IV-Object-Detection/data_loader.py
@@ -140,3 +140,30 @@ class TacoDataset(torch.utils.data.Dataset):
                                 labels=target["labels"])
 
         return x['image'], x['bboxes'], x['labels']
+    
+    def collate_fn(self, batch):
+        """
+        Since each image may have a different number of objects, we need a collate function (to be passed to the DataLoader).
+
+        This describes how to combine these tensors of different sizes. We use lists.
+
+        Note: this need not be defined in this Class, can be standalone.
+
+        :param batch: an iterable of N sets from __getitem__()
+        :return: a tensor of images, lists of varying-size tensors of bounding boxes, labels, and difficulties
+        """
+
+        images = list()
+        boxes = list()
+        labels = list()
+        # difficulties = list()
+
+        for b in batch:
+            images.append(b[0])
+            boxes.append(b[1])
+            labels.append(b[2])
+            # difficulties.append(b[3])
+
+        images = torch.stack(images, dim=0)
+
+        return images, boxes, labels # , difficulties  # tensor (N, 3, 300, 300), 3 lists of N tensors each
