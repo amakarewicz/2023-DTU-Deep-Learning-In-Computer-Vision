f9de91652350817ada0ad505a6e620fdb6c45b05
Tobias Munch <s183478@student.dtu.dk>
2023-06-19 22:03:17 +0200

    Some refactoring made

--- IV-Object-Detection/config.py
+++ IV-Object-Detection/config.py
@@ -1,5 +1,12 @@
+import torch.nn as nn
+
 NUM_IMAGES = 1500
 BATCH_SIZE = 16
+IMG_SIZE = 256
+EPOCHS = 100
+LR = 1e-4
+DROP_OUT_RATE = 0.5
+LOSS_FN = nn.BCELoss()
 
 data_path = '/dtu/datasets1/02514/data_wastedetection/'
 anns_file_path = data_path + '/' + 'annotations.json'
